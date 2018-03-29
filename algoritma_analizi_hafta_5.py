c9a6e80ab68bc1d34cdc3bd830efa265020a4e1f
kaanylmz <kaanyilmaz017@gmail.com>
2018-03-29 22:09:00 +0200

    Hafta 5

---
+++ algoritma_analizi_hafta_5.py
@@ -0,0 +1,11 @@
+import sys
+my_L_1=[]
+size_1=sys.getsizeof(my_L_1)
+print("baþlangýç boyutu : ",size_1,end="")
+print()
+for i in range(20000):
+    my_L_1.append(str(i)+" test")
+    if(size_1!=sys.getsizeof(my_L_1)):
+        print("dizi yer deðiþtirdi , amortized cost ",end=" ")
+        size_1=sys.getsizeof(my_L_1)
+        print("size: ",size_1)
